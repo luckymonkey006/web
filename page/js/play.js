6c27f6977fe04058f477db9fad00793ea45a1296
零零六 <luckymonkey006@gmail.com>
2019-05-15 18:22:31 +0800

    a

---
+++ page/js/play.js
@@ -0,0 +1,12 @@
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]);
+    return null; //返回参数值
+}
+
+var curWwwPath = window.location.href;
+var pathName = window.location.pathname;
+var pos = curWwwPath.indexOf(pathName);
+var localhostPaht = curWwwPath.substring(0, pos + 5);
+var torrentId = localhostPaht + '/res/' + getUrlParam("torrentId") + "/info.torrent"
